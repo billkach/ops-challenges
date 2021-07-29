ca2cdb0ec97a0c60080fd0b8707235030338310a
billkach <billkach@gmail.com>
2021-07-29 02:08:49 -0700

    Added shell script for functions

---
+++ funktion1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Script Name:                             Ops Challenge 03; Functions
+# Author:                                  Bill Kachersky
+# Date of last revision                    7/28/2021
+
+
+# This script is meant to explore
+# how to create functions, and how
+# to contain variables in them
+
+# FUNCTION
+WhoCameBefore() {
+ last $USER
+}
+
+echo WhoCameBefore
+
