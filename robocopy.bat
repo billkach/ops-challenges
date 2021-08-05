a592cee7083417eb6790bfad3c9de8bfb8967c15
billkach <billkach@gmail.com>
2021-08-05 16:55:00 +0000

    This is where I'm at with this, I don't know how to make this work.

---
+++ robocopy.bat
@@ -0,0 +1,11 @@
+
+
+:: Script Name                           My First Loop
+:: Author                                Bill Kachersky
+:: Date of last revision                 07/30/2021
+:: Description of purpose                To Create and Test a While Loop, with USER INTERACTION!
+
+
+@echo off
+ROBOCOPY C:\Users\j.thompson\Desktop\Data E:\Backups /LOG:backuplog.txt
+pause
