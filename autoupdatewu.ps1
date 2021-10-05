ad830d2307a381e7054dbb32bba1ab6ba9bd9840
billkach <billkach@gmail.com>
2021-10-04 22:53:32 -0700

    Windows Auto Update ver2

---
+++ autoupdatewu.ps1
@@ -0,0 +1,16 @@
+﻿# Script Name                       autoupdatewu.ps1
+# Author                            Bill Kachersky
+# Date of last revision             10/04/2021
+# Description of purpose            Ops 401 Lab 01 script component
+
+
+# Main
+
+
+$AUSettings=(New-Object -com "Microsoft.Update.AutoUpdate").Settings
+
+$AUSettings.NotificationLevel=2
+
+$AUSettings.FeaturedUpdatesEnabled=True
+
+$AUSettings.Save()
