74d63532fbf5307756727e885aab8bbc17f43f42
billkach <billkach@gmail.com>
2021-10-04 22:52:52 -0700

    Windows Auto Update ver1

---
+++ autoupdate.ps1
@@ -0,0 +1,21 @@
+﻿# Script Name                       autoupdate.ps1
+# Author                            Bill Kachersky
+# Date of last revision             10/04/2021
+# Description of purpose            Ops 401 Lab 01 script component
+
+
+# Main
+
+set-executionpolicy -ExecutionPolicy remotesigned -scope Process -force
+
+Write-Host "Let's automate the installation of Windows Updates on this machine"
+
+Start-Sleep -Seconds 1
+
+Import-Module -Name PSWindowsUpdate
+
+Install-Module -Name PSWindowsUpdate
+
+Get-WindowsUpdate
+
+Get-WUInstall -AcceptAll -AutoReboot
