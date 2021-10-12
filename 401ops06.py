8e61f25a9567f0fe17f8568e39a3483782ef13b5
billkach <billkach@gmail.com>
2021-10-12 01:29:47 -0700

    update: challenge completed.

--- 401ops06.py
+++ 401ops06.py
@@ -7,9 +7,11 @@
 
 
 # Imports
+import time
 from cryptography.fernet import Fernet
 
 
+# Functions
 def write_key():
     """
     Generates a key and save it into a file
@@ -24,70 +26,116 @@ def load_key():
     """
     return open("key.key", "rb").read()
 
-def encrypt(filename, key):
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        file_data = file.read()
-        encrypted_data = f.encrypt(file_data)
-    with open(filename, "wb") as file:
-         file.write(encrypted_data)
-
-
-def stren():
-    msg = input("Type your message: ")
-    message = msg.encode()
-    f = Fernet(key)
-    encrypted = f.encrypt(message)
-    print("Alright, encrypted! Let's see how it looks...")
-    print(encrypted)
-
-
-
-
-
-
 def main():
     while True:
         slct ='0'
         while slct =='0':
-            print("Main Choice: Choose 1 of 5 choices")
-            print("Choose 1 for something")
-            print("Choose 2 for something")
-            print("Choose 3 for something")
-            print("Choose 4 for something")
-            print("Choose 5 to go to another menu")
+            print("Choose 1 of 5 choices")
+            time.sleep(1)
+            print("1. Encrypt a file")
+            print("2. Decrypt a file")
+            print("3. Encrypt a message")
+            print("4. Decrypt a message")
+            print("5. Exit Menu")
+            time.sleep(1)
 
   
     ## Displays menu
             slct = input("Enter your choice [1-5]: ")
      
-            if slct == "1":     
-                print("Alright, let's encrypt a file!")
-                write_key()
+            if slct == "1":
                 key = load_key()
+                f = Fernet(key)
+                print("Alright, let's encrypt a file!")
+                time.sleep(1)
                 filename = input("Please input the filename of the file in the current directory you would like to encrypt: ")
-                encrypt(filename, key)
-        
+                time.sleep(1)
+                with open(filename, "rb") as file:
+                    file_data = file.read()
+                    encrypted_data = f.encrypt(file_data)
+                with open(filename, "wb") as file:
+                    file.write(encrypted_data)
+                    time.sleep(1)
+                print("Encrypted! What next?")
+                time.sleep(1)
+                main()
         
             elif slct == "2":
+                key = load_key()
+                f = Fernet(key)
                 print("Alright, let's decrypt a file!")
-        ## You can add your code or functions here
-            elif slct==3:
-                print("Alright, let's encrypt a message!")
                 write_key()
                 key = load_key()
+                filename = input("Please input the filename of the file in the current directory you would like to decrypt: ")
+                time.sleep(1)
+                with open(filename, "rb") as file:
+                    encrypted_data = file.read()
+                    decrypted_data = f.decrypt(encrypted_data)
+                with open(filename, "wb") as file:
+                    file.write(decrypted_data)
+                time.sleep(1)
+                print("Decrypted! What next?")
+                time.sleep(1)
+                main()
+
+            elif slct == "3":
+                key = load_key()
+                f = Fernet(key)
+                print("Alright, let's encrypt a message!")
+                time.sleep(1)
+                msg = input("Type your message: ")
+                time.sleep(1)
+                message = msg.encode()
+                encrypted = f.encrypt(message)
+                print("Alright, encrypted! Let's see how it looks...")
+                print(encrypted)
+                dcrpt = input("Would you like to decrypt this message too? Y/N: ")
+                if dcrpt == "Y":
+                    de = f.decrypt(encrypted)
+                    print(de)
+                else:
+                     print("What would you like to do next?")
+                     main()
                 
 
-            elif slct==4:
+            elif slct == "4":
+                key = load_key()
+                f = Fernet(key)
                 print("Alright, let's decrypt a message!")
-        ## You can add your code or functions here
-            elif slct==5:
+                time.sleep(1)
+                print("First, we need to encrypt a message, though :D")
+                time.sleep(1)
+                msgt = input("Type your message: ")
+                time.sleep(1)
+                message = msgt.encode()
+                encrypted = f.encrypt(message)
+                print("Alright, encrypted! Let's see how it looks...")
+                print(encrypted)
+                print("Encrypted!")
+                time.sleep(1)
+                print("Now, we'll decrypt it.")
+                time.sleep(1)
+                decrypted_encrypted = f.decrypt(encrypted)
+                print(decrypted_encrypted)
+                time.sleep(1)
+                print("What would you like to do next?")
+                main()
+
+            elif slct == "5":
+                time.sleep(1)
                 print("See ya!")
-        ## You can add your code or functions here
-                loop=False # This will make the while loop to end as not value of loop is set to False
+                exit()
+
             else:
         # Any integer inputs other than values 1-5 we print an error message
                 input("Wrong option selection. Enter any key to try again..")
 
 
+# Main
+
+write_key()
+print("Welcome to the encrypt/decrypt-ionary!!!")
+time.sleep(1)
 main()
+
+# End
